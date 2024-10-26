65cb4914765562dcdcff1f7b8f29f887c12cc528
Hyunjin Kim <70287773+hjhyunjinkim@users.noreply.github.com>
2024-10-26 13:24:58 -0700

    Add files via upload

---
+++ rb5_ros2-fa24_cse276a/rb5_ros2_control/src/homework2.py
@@ -0,0 +1,146 @@
+import sys
+import rclpy
+from rclpy.node import Node
+
+from geometry_msgs.msg import Twist
+import numpy as np
+import math
+import time
+
+import tf 
+import tf2_ros
+from tf.transformations import quaternion_matrix
+
+class PIDcontroller(Node):
+    def __init__(self, Kp, Ki, Kd):
+        super().__init__('PID_Controller_NodePub')
+        self.Kp = Kp
+        self.Ki = Ki
+        self.Kd = Kd
+        self.target = None
+        self.I = np.array([0.0, 0.0, 0.0])
+        self.lastError = np.array([0.0, 0.0, 0.0])
+        self.timestep = 0.1
+        self.maximumValue = 0.1
+        self.publisher_ = self.create_publisher(Twist, '/twist', 10)
+
+    def setTarget(self, targetx, targety, targetw):
+        """
+        set the target pose.
+        """
+        self.I = np.array([0.0,0.0,0.0]) 
+        self.lastError = np.array([0.0,0.0,0.0])
+        self.target = np.array([targetx, targety, targetw])
+
+    def setTarget(self, state):
+        """
+        set the target pose.
+        """
+        self.I = np.array([0.0,0.0,0.0]) 
+        self.lastError = np.array([0.0,0.0,0.0])
+        self.target = np.array(state)
+
+    def getError(self, currentState, targetState):
+        """
+        return the different between two states
+        """
+        result = targetState - currentState
+        result[2] = (result[2] + np.pi) % (2 * np.pi) - np.pi
+        return result 
+
+    def setMaximumUpdate(self, mv):
+        """
+        set maximum velocity for stability.
+        """
+        self.maximumValue = mv
+
+    def update(self, currentState):
+        """
+        calculate the update value on the state based on the error between current state and target state with PID.
+        """
+        e = self.getError(currentState, self.target)
+
+        P = self.Kp * e
+        self.I = self.I + self.Ki * e * self.timestep 
+        I = self.I
+        D = self.Kd * (e - self.lastError)
+        result = P + I + D
+
+        self.lastError = e
+
+        # scale down the twist if its norm is more than the maximum value. 
+        resultNorm = np.linalg.norm(result)
+        if(resultNorm > self.maximumValue):
+            result = (result / resultNorm) * self.maximumValue
+            self.I = 0.0
+
+        return result
+    
+def genTwistMsg(desired_twist):
+    """
+    Convert the twist to twist msg.
+    """
+    twist_msg = Twist()
+    twist_msg.linear.x = desired_twist[0] 
+    twist_msg.linear.y = desired_twist[1] 
+    twist_msg.linear.z = 0.0
+    twist_msg.angular.x = 0.0
+    twist_msg.angular.y = 0.0
+    twist_msg.angular.z = desired_twist[2]
+    return twist_msg
+
+def coord(twist, current_state):
+    J = np.array([[np.cos(current_state[2]), np.sin(current_state[2]), 0.0],
+                  [-np.sin(current_state[2]), np.cos(current_state[2]), 0.0],
+                  [0.0,0.0,1.0]])
+    return np.dot(J, twist)
+
+def get_visual_feedback(l):
+    # get the current location of the robot
+    
+    pass
+
+
+if __name__ == "__main__":
+    rclpy.init()
+    waypoints = np.array([[0.0, 0.0, 0.0],
+                          [1.0, 0.0, 0.0],
+                          [1.0, 2.0, math.pi],
+                          [0.0, 0.0, 0.0]])
+    
+    pid = PIDcontroller() #Need to tune Kp, Ki, Kd
+
+    current_state = np.array([0.0, 0.0, 0.0])
+
+    for wp in waypoints:
+        print("move to waypoint", wp)
+        #set wp as the target point
+        pid.setTarget(wp)
+        update_value = pid.update(current_state)
+        # publish the twist
+        pid.publisher_.publish(genTwistMsg(coord(update_value, current_state)))
+        time.sleep(0.05)
+        #update the current state
+        current_state += update_value
+        detected_tag, estimate_state = get_visual_feedback()
+        if detected_tag:
+            current_state = estimate_state
+            
+        while(np.linalg.norm(pid.getError(current_state, wp)) > 0.05): # check the error between current state and current way point
+            # calculate the current twist
+            update_value = pid.update(current_state)
+            # publish the twist
+            pid.publisher_.publish(genTwistMsg(coord(update_value, current_state)))
+            #print(coord(update_value, current_state))
+            time.sleep(0.05)
+            # update the current state
+            current_state += update_value   
+            detected_tag, estimate_state = get_visual_feedback()
+            if detected_tag:
+                current_state = estimate_state
+
+    pid.publisher_.publish(genTwistMsg(np.array([0.0,0.0,0.0])))
+        
+
+
+    
